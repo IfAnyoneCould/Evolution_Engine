657ee1a9b1a4a7e3a375c6304fa6e001f1e919a2
IfAnyoneCould <snarkx8201@gmail.com>
2026-09-11 14:34:31 -0400

    added the RunBatch function to evaluate and entire generation, to do next is creating the next generation

--- internal/population/population.go
+++ internal/population/population.go
@@ -3,6 +3,11 @@ package population
 import (
 	"Evolution_Engine/internal/config"
 	"Evolution_Engine/internal/genome"
+	"cmp"
+	"context"
+	"errors"
+	"slices"
+	"sync"
 )
 
 type Agent struct {
@@ -54,4 +59,57 @@ func NewPopulation(count int, path string) (*Population, error) {
 	}
 
 	return &Population{agents, prog, nudge}, nil
+}
+
+func (p *Population) RunBatch(ctx context.Context, workers int) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	agents := make(chan *Agent, len(p.Agents))
+	for i := range p.Agents {
+		agents <- &p.Agents[i]
+	}
+	close(agents)
+
+	errs := make(chan error)
+
+	wg := sync.WaitGroup{}
+	for range workers {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for a := range agents {
+				err := a.Evaluate(p.Prog)
+				if err != nil {
+					select {
+					case errs <- err:
+					case <-ctx.Done():
+					}
+					cancel()
+					return
+				}
+				if ctx.Err() != nil {
+					return
+				}
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(errs)
+	}()
+
+	var errList []error
+	for e := range errs {
+		errList = append(errList, e)
+	}
+
+	return errors.Join(errList...)
+}
+
+func (p *Population) Rank() {
+	slices.SortFunc(p.Agents, func(a, b Agent) int {
+		return cmp.Compare(a.Fitness, b.Fitness)
+	})
 }
